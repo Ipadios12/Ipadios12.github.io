72f208749bd94b393749ffdedf4a4de71668ba9e
Ipadios12 <corinaescu2015@gmail.com>
2023-01-12 20:04:36 +0200

    java update

--- index.js
+++ index.js
@@ -1,56 +1,48 @@
 var skillsEl = document.getElementById("skills-list");
 
-var skills = ["HTML", "CSS", "JS","Drive"];
+var skills = ["HTML", "CSS", "JS", "Drive"];
 
 var skillsHTML = "";
 var i = 0;
 
 for (var i = 0; i < skills.length; ) {
-skillsHTML += "<li>" + skills[i] + "</li>";
-i++;
+  skillsHTML += "<li>" + skills[i] + "</li>";
+  i++;
 }
 
 skillsEl.innerHTML = skillsHTML;
 
-var pageIds = ["home", "skills", "languages", "projects",];
+var pageIds = ["home", "skills", "languages", "projects"];
 
 function hideAllPages() {
-  pageIds.forEach(function(pageId) {
+  pageIds.forEach(function (pageId) {
     hide(pageId);
   });
-   
-  }
-  
-  function show(id) {
-    document.getElementById(id).style.display = "block";
-  }
-  
-  function hide(id) {
-     document.getElementById(id).style.display = "none";
-  }
-
-  function displayhome() {
-    hideAllPages();
-    show("home");
-  }
-  function displayskills() {
-    hideAllPages();
-    show("skills");
-  }
-  function displaylanguages() {
-    hideAllPages();
-    show("languages");
-  }
-  function displayprojects() {
-    hideAllPages();
-    show("projects");
-  }
-  
-{
-    function displayprojects() {
-        hideAllPages();
-        show("projects");
-    }
 }
-  displayhome();
-  
+
+function show(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function hide(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+function displaylanguages() {
+  displayPage("languages");
+}
+function displayprojects() {
+  displayPage("projects");
+}
+
+{
+  function displayprojects() {
+    displayPage("projects");
+  }
+}
+function displayPage(id) {
+  hideAllPages();
+  show(id);
+}
+
+displayPage('home');
