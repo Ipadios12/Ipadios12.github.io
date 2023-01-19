0f5412bb24319a0c6cd99841e1a328bfdd7d8189
Ipadios12 <corinaescu2015@gmail.com>
2023-01-19 21:48:58 +0200

    update

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 var skillsEl = document.getElementById("skills-list");
 
-var skills = ["HTML", "CSS", "JS", "Drive"];
+var skills = [
+  { name: "HTML", endorcements: 5 },
+  { name: "CSS", endorcements: 4 },
+  { name: "JS", endorcements: 4 },
+  { name: "Drive", endorcements: 2 },
+];
+//var skills = [["HTML", 5], ["CSS", 3], "JS", "Drive"];
+// var endorcements = [5, 6, 5, 1];
 
-var skillsHTML = "";
-var i = 0;
+var skillsHTML = skills.map(function (skill) {
+  return `<li>${skill.name} - <span>${skill.endorcements}</span></li>`;
+});
 
-for (var i = 0; i < skills.length; ) {
-  skillsHTML += "<li>" + skills[i] + "</li>";
-  i++;
-}
-
-skillsEl.innerHTML = skillsHTML;
+skillsEl.innerHTML = skillsHTML.join("");
 
 function hideAllPages() {
   var pages = document.querySelectorAll(".page");
   pages.forEach(function (page) {
     hide(page.id);
   });
-  
 }
 
 function show(id) {
   document.getElementById(id).style.display = "block";
 }
-
 function hide(id) {
   document.getElementById(id).style.display = "none";
 }
@@ -33,4 +34,17 @@ function displayPage(id) {
   show(id);
 }
 
-displayPage("home");
+function initMenu() {
+  document
+    .getElementById("top-menu-bar")
+    .addEventListener("click", function (e) {
+      if (e.target.matches("a")) {
+        // var id = e.target.getAttribute("data-page");
+        var id = e.target.dataset.page;
+        displayPage(id);
+      }
+    });
+}
+
+displayPage("skills");
+initMenu();
