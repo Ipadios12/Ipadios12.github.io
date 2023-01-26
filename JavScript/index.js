94ec91358968af93183301777567cf8991d0d63e
Valentin <Corinaescu2015@gmail.com>
2023-01-26 21:34:00 +0200

    uo

--- JavScript/index.js
+++ JavScript/index.js
@@ -1,25 +1,22 @@
-const skillsEl = document.getElementById("skills-list");
-
 const r1 = fetch("/JavScript/skills.json");
-r1.then(function (raspuns) {
+r1.then((raspuns) => {
   const r2 = raspuns.json();
-  r2.then(function (skills) {
+  r2.then((skills) => {
     displaySkills(skills);
   });
 });
 
 function displaySkills(skills) {
-  const skillsHTML = skills.map(function (skill) {
+  const skillsHTML = skills.map((skill) => {
     return `<li>${skill.name} - <span>${skill.endorcements}</span></li>`;
   });
-
   const skillsEl = document.getElementById("skills-list");
   skillsEl.innerHTML = skillsHTML.join("");
 }
 
 function hideAllPages() {
   const pages = document.querySelectorAll(".page");
-  pages.forEach(function (page) {
+  pages.forEach((page) => {
     hide(page.id);
   });
 }
@@ -37,19 +34,21 @@ function displayPage(id) {
 }
 
 function initMenu() {
-  document
-    .getElementById("top-menu-bar")
-    .addEventListener("click", function (e) {
-      if (e.target.matches("a")) {
-        // var id = e.target.getAttribute("data-page");
-        const id = e.target.dataset.page;
-        displayPage(id);
-      }
-    });
+  document.getElementById("top-menu-bar").addEventListener("click", (e) => {
+    if (e.target.matches("a")) {
+      // var id = e.target.getAttribute("data-page");
+      const id = e.target.dataset.page;
+      displayPage(id);
+    }
+  });
 }
 
 displayPage("home");
-
 initMenu();
 
-console.warn("end of file");
+const f1 = function () {
+  return "arrow";
+};
+const f2 = () => {
+  return "arrow";
+};
